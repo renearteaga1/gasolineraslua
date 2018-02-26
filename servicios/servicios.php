da0f2c883767d564bb736acd64f4ac5189ee7eb5
Rene <renev88@gmail.com>
2018-02-26 13:26:50 -0500

    First layout for solicitud credito

--- servicios/servicios.php
+++ servicios/servicios.php
@@ -2,7 +2,7 @@
 date_default_timezone_set('America/Bogota');
 
     if (isset($_POST['submit'])) {
-        
+
         include "db.php";
         include "functions.php";
 
@@ -42,7 +42,7 @@ $saldoAnterior = $row2['SUM(Haber)'] - $row2['SUM(Debe)'];
       body {
         padding-top: 54px;
       }
-      
+
       .cuenta-cliente {
         background-color: rgba(237, 237, 237, .8);
         padding-bottom: 2rem;
@@ -63,7 +63,7 @@ $saldoAnterior = $row2['SUM(Haber)'] - $row2['SUM(Debe)'];
       .comentario-mobile {
           font-size: .8rem;
       }
-      
+
       .card-margin {
           margin: 10px;
       }
@@ -105,13 +105,13 @@ tbody tr:nth-child(even) td {
         .card-padd {
             margin: 1.5rem;
         }
-        .table-mobile th, .table-mobile td { 
-             border-top: none !important; 
+        .table-mobile th, .table-mobile td {
+             border-top: none !important;
          }
         .table-mobile tr {
         background-color: rgba(242, 242, 242, .3);
         }
-        
+
         .table-mobile tr:nth-child(4n+1), tr:nth-child(4n+2) {
             background-color: rgba(229, 229, 229, .8);;
         }
@@ -126,7 +126,7 @@ tbody tr:nth-child(even) td {
             padding:0;
         }
         .comentario {
-            
+
             padding: 0;
         }
       }
@@ -142,22 +142,22 @@ tbody tr:nth-child(even) td {
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="">GasolinerasLua S.A.</a>
+        <a class="navbar-brand" href="../index.php">GasolinerasLua S.A.</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="#">Inicio
+              <a class="nav-link" href="../index.php">Inicio
                 <span class="sr-only">(current)</span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/nosotros/nosotros.php">Nosotros</a>
+              <a class="nav-link" href="#">Nosotros</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/servicios/servicios.php">Servicios</a>
+              <a class="nav-link" href="#">Servicios</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Contacto</a>
@@ -169,123 +169,65 @@ tbody tr:nth-child(even) td {
 
     <!-- Page Content -->
     <div class="container">
-      
+
         <div class="col text-center" >
           <!--<h1 class="mt-5">Garantia en Calidad y Cantidad</h1>-->
-          
-          <img src="Petroecuador.png" class="img-fluid logo" alt="Petroecuador">
+
+          <img src="../Petroecuador.png" class="img-fluid logo" alt="Petroecuador">
           <p class="lead"></p>
           <ul class="list-unstyled">
             <li></li>
           </ul>
         </div>
-            <?php 
-                include 'Mobile_Detect.php';
-                $detect = new Mobile_Detect();
-            
-                // Check for any mobile device.
-                if ($detect->isMobile()) {
-                 // mobile content
-                    echo "<div>";
-                        
-                            if (isset($_POST['submit'])) {
-                       readClientId();
-                            }
-                
-                //<!--<a href="#" class="card-link">Card link</a>
-                //<a href="#" class="card-link">Another link</a>-->
-            echo "</div>";
 
-       
-            echo "<div class=''>";
-                
-                    if (isset($_POST['submit'])) {
-                        tableHeaderMobile();
-                    }
-                
-            echo "</div>";
-    
-            echo "<div >";
-                
-                    if (isset($_POST['submit'])) {
-                        clientDataMobile();
-                    }
-                
-            echo "</div>";
-                 
-                
-                }else {
-                 // other content for desktops
-           echo "<div>";
-                        
-                            if (isset($_POST['submit'])) {
-                       readClientId();
-                            }
-                
-                //<!--<a href="#" class="card-link">Card link</a>
-                //<a href="#" class="card-link">Another link</a>-->
-            echo "</div>";
-
-       
-            echo "<div class=''>";
-                
-                    if (isset($_POST['submit'])) {
-                        tableHeader();
-                    }
-                
-            echo "</div>";
-    
-            echo "<div >";
-                
-                    if (isset($_POST['submit'])) {
-                        clientData();
-                    }
-                
-            echo "</div>";
-            } ?>
-            
-        <!--
-        <div class='row'>
-            <div class= "col" style="background-color:#cccccc">Blue
-            </div>
-        </div> -->
-      <!--  
-        <table class="table table-striped">
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Username</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>2017-Oct-02</td>
-      <td>FACT 007002 0015652 COMPRA 1 ACOPLE Y 1 INFLADOR GTO E/S ANT II</td>
-      <td>2,630.43</td>
-      <td>2,630.43</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table> -->
-             
         <div class='row'>
         <div class= "col mt-sm-3 text-center cuenta-cliente">
-          <h2 class="mt-5">Estado de cuenta de clientes</h2>
-          <p class="lead">Ingrese su RUC o CI y seleccione el rango de fechas de su consulta:</p>
-         <form class="form-inline justify-content-center" action="index.php" method="post">
+          <h2 class="mt-5">Solicitud de Crédito</h2>
+        </div>
+        </div>
+        <div class="row"
+        <div class="col"
+          <p class="lead ">Por favor llenar los siguientes campos:</p>
+          <p class="text">Estimado cliente, para poder ofrecerle un servicio basado en la seguridad y confianza, solicitamos a usted muy comedidamente se sirva hacernos llegar los siguientes documentos:</p>
+        </div>
+        <div class="row">
+        <div class="col">
+        <div id="accordion">
+<div class="card">
+  <div class="card-header" id="headingOne">
+    <h5 class="mb-0">
+      <button class="btn btn-link card-title" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Persona Natural
+      </button>
+    </h5>
+  </div>
+
+  <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+    <div class="card-body">
+      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+    </div>
+  </div>
+</div>
+<div class="card">
+  <div class="card-header" id="headingTwo">
+    <h5 class="mb-0">
+      <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Collapsible Group Item #2
+      </button>
+    </h5>
+  </div>
+  <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+    <div class="card-body">
+      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+    </div>
+  </div>
+</div>
+</div>
+</div>
+        </div>
+        </div>
+        <div class= "col mt-sm-3 text-center cuenta-cliente">
+         <form class="form-inline justify-content-center" action="10-webapp/compras.php" method="post">
             <label class="sr-only" for="inlineFormInput">RUC/CI</label>
             <input type="text" class="form-control mb-2 mr-sm-3 mb-sm-2" name="nombre" id="inlineFormInput" placeholder="RUC/CI">
 
@@ -294,7 +236,7 @@ tbody tr:nth-child(even) td {
               <div class="input-group-addon">Incial</div>
               <input type="date" class="form-control" name="fechaInicial" id="inlineFormInputGroup" value="2017/01/01">
             </div>
-            
+
             <label class="sr-only" for="inlineFormInputGroup">Fecha Final</label>
             <div class="input-group mb-2 mr-sm-3 mb-sm-2">
               <div class="input-group-addon">Final</div>
